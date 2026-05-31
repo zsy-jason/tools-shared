ff47fee7d41ee3e8e8561041b1ce2c8b50e923ea
zsy-jason <186247581+zsy-jason@users.noreply.github.com>
2026-05-31 22:39:08 +0800

    [Infra] Create Gemfile before executing bundle install.

--- gen_ios_pkg.py
+++ gen_ios_pkg.py
@@ -175,6 +175,12 @@ def main():
     source_dirs = ["build"]
 
     print("run generate_podspec")
+    gemfile_content = """source 'https://rubygems.org'
+        gem "cocoapods", '1.11.3'
+        gem "ffi", "1.16.3"
+    """
+    with open("Gemfile", "w") as f:
+        f.write(gemfile_content)
     run_command(
         f"SDKROOT=/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk bundle install --path {args.cache_path}"
     )
